cd7e66a774180cbff1faf965274c9393cf0bd69a
NguyenHao2310 <haogamerpq@gmail.com>
2025-12-22 21:58:40 +0700

    SQL
    Kết nối sql bằng chuỗi thông qua tailscale

---
+++ ProjectN4/DAL/DatabaseHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ProjectN4.DAL
+{
+    public class DatabaseHelper
+    {
+        // Tạo chuỗi kết nối từ thông tin bên file DbSettings
+        public static string ConnectionString = $"Data Source={DbSettings.ServerIP},1433;Initial Catalog={DbSettings.DatabaseName};User ID={DbSettings.UserID};Password={DbSettings.Password};Network Library=DBMSSOCN;";
+
+        // Hàm lấy kết nối
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            try
+            {
+                conn.Open();
+                return conn;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Lỗi kết nối Server: " + ex.Message, "Lỗi Kết Nối", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        // Hàm kiểm tra kết nối (Test)
+        public static bool TestConnection()
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ ProjectN4/DAL/DbSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectN4.DAL
+{
+    internal class DbSettings
+    {
+        public static string ServerIP = "100.65.72.80";
+
+        // 2. Tên Database (Theo file SQL)
+        public static string DatabaseName = "QuanLyChuoiKS_NH";
+
+        // 3. Tài khoản SQL (Hỏi bạn của bạn)
+        public static string UserID = "SQLKhachSan";
+
+        // 4. Mật khẩu SQL (Hỏi bạn của bạn)
+        public static string Password = "123456";
+    }
+
+}
